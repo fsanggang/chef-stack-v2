2b8531c120b7fc2dedfde243ab104104f81e7ec0
Fiona Sanggang <fsanggang@gmail.com>
2015-10-04 20:08:21 +1300

    Modify chef-master.sh to allow `vagrant destroy` and `vagrant up`
    * Check for presence of pivotal.rb before (re)running chef-server-ctl reconfigure
    * Add commands to create user and organisation into chef-master.sh
    
    This should let me run `vagrant destroy` and `vagrant up` and get back to
    this point.

--- chef-master.sh
+++ chef-master.sh
@@ -2,6 +2,9 @@ if ! dpkg --list chef-server-core > /dev/null 2>&1; then
   sudo dpkg -i /vagrant/chef-server-core_12.2.0-1_amd64.deb;
 fi
 
-if chef-server-ctl status | awk '{print $1}' | grep run -q; then
+if ! [ -e /etc/opscode/pivotal.rb ]; then
   chef-server-ctl reconfigure
 fi
+
+chef-server-ctl user-create admin admin admin fiona@showgizmo.com password --filename /vagrant/files/admin.pem
+chef-server-ctl org-create chef-stack-demo "chef-stack-demo" --association_user admin --filename /vagrant/files/validator.pem
