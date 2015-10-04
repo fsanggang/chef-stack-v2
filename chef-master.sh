36333b591f23174fc55e137b8b9a0dd0d4bc87cb
Fiona Sanggang <fsanggang@gmail.com>
2015-10-04 19:29:05 +1300

    Reconfigure chef-master if necessary.

--- chef-master.sh
+++ chef-master.sh
@@ -1,3 +1,7 @@
 if ! dpkg --list chef-server-core > /dev/null 2>&1; then
   sudo dpkg -i /vagrant/chef-server-core_12.2.0-1_amd64.deb;
 fi
+
+if chef-server-ctl status | awk '{print $1}' | grep run -q; then
+  chef-server-ctl reconfigure
+fi
